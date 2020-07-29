abae402ce9a6747a2fcee42bd12635fdb039ebaa
Mayank Jain <jainmayank16041999@gmail.com>
2020-07-29 14:18:13 +0530

    Setup for heroku done

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-//console.log("Client side JS");
-
-// fetch("http://puzzle.mead.io/puzzle")
-//     .then((res) => {
-//         res.json().then((data) => {
-//             console.log(data);
-//         })
-//     })
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 
@@ -22,7 +13,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:8000/weather?address=${location}`).then((res) => {
+  fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -1,108 +1,105 @@
-const path = require("path")
-const express = require("express")
-const hbs = require("hbs")
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 8000;
 
-var geocode = require('./utils/geocode');
-const openWeather = require("./utils/openWeather")
+var geocode = require("./utils/geocode");
+const openWeather = require("./utils/openWeather");
 
 //path for express config
-const publicDirPath = path.join(__dirname, "../public")
-const viewPath = path.join(__dirname, "../templates/views")
-const partialsPath = path.join(__dirname, "../templates/partials")
+const publicDirPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //setup hanbar engine and view location
-app.set("view engine", "hbs")
-app.set("views", viewPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
 
 //static directory to serve
-app.use(express.static(publicDirPath))
-
+app.use(express.static(publicDirPath));
 
 app.get("", (req, res) => {
-    res.render("index", {
-        title: "Weather",
-        name: "Mayank",
-    })
-})
-
+  res.render("index", {
+    title: "Weather",
+    name: "Mayank",
+  });
+});
 
 app.get("/about", (req, res) => {
-    res.render("about", {
-        title: "About",
-        name: "Mayank"
-    })
-})
+  res.render("about", {
+    title: "About",
+    name: "Mayank",
+  });
+});
 
 app.get("/help", (req, res) => {
-    res.render("help", {
-        title: "Help",
-        name: "Mayank"
-    })
-})
+  res.render("help", {
+    title: "Help",
+    name: "Mayank",
+  });
+});
 
 app.get("/weather", (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: "Please provide address"
-        })
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide address",
+    });
+  }
+  geocode(req.query.address, (err, data) => {
+    if (err) {
+      return res.send({
+        error: err,
+      });
     }
-    geocode(req.query.address, (err, data) => {
-        if (err) {
-            return res.send({
-                error: err
-            })
-        }
 
-        const { latitude = undefined, longitude = undefined, location } = data
+    const { latitude = undefined, longitude = undefined, location } = data;
 
-        openWeather(latitude, longitude, (err, weatherData) => {
-            if (err) {
-                return res.send({
-                    error: err
-                })
-            }
-            return res.send({
-                forcast: weatherData,
-                location,
-            })
-        })
-    })
-
-})
+    openWeather(latitude, longitude, (err, weatherData) => {
+      if (err) {
+        return res.send({
+          error: err,
+        });
+      }
+      return res.send({
+        forcast: weatherData,
+        location,
+      });
+    });
+  });
+});
 
 app.get("/products", (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: "Provide search message"
-        })
+  if (!req.query.search) {
+    return res.send({
+      error: "Provide search message",
+    });
+  }
 
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
 
 app.get("/help/*", (req, res) => {
-    res.render("notFound", {
-        title: "404",
-        errorMessage: "Help article not found",
-        name: "Mayank"
-    })
-})
+  res.render("notFound", {
+    title: "404",
+    errorMessage: "Help article not found",
+    name: "Mayank",
+  });
+});
 
 app.get("*", (req, res) => {
-    res.render("notFound", {
-        title: "404",
-        errorMessage: "Page not found",
-        name: "Mayank"
-    })
-})
+  res.render("notFound", {
+    title: "404",
+    errorMessage: "Page not found",
+    name: "Mayank",
+  });
+});
 
-app.listen(8000, () => {
-    console.log("Server is up on port 8000");
-})
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
